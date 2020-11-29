4da567b5731bb91c60d5548ef100ee57a34bccdf
Dominic Quigley <djq2@illinois.edu>
2020-11-29 00:07:13 -0600

    Progress

--- Code/Graph.cpp
+++ Code/Graph.cpp
@@ -4,4 +4,11 @@ Graph::Graph(std::vector<Airport> airports, std::vector<Route> routes){
     int i = 0;
     std::ifstream airport_data;
     std::ifstream route_data;
+    while(!airport_data.eof()){
+
+    }
+
+    while(!route_data.eof()){
+        
+    }
 }
