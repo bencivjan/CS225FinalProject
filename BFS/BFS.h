ad9740bd25637311f6af917c106044e2690aeeae
prestonchao <prestonchao1@gmail.com>
2020-12-05 17:11:08 -0600

    Added template for BFS and changed data to CSV

---
+++ BFS/BFS.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <queue>
+
+#include "../cs225/PNG.h"
+
+class BFS {
+   public:
+    BFS(const PNG& png, const Point& start, double t);
+
+   private:
+};
--- main.cpp
+++ main.cpp
@@ -7,30 +7,39 @@ int main() {
     std::unordered_map<std::string, std::pair<Airport, std::vector<Route>>>
         airports = FlightMap.get_airports();
     std::vector<Route> routes = FlightMap.get_routes();
-    // std::cout << "Left constructor\n";
-    std::unordered_map<std::string,
-                       std::pair<Airport, std::vector<Route>>>::iterator it;
-    std::cout << "Pointer: " << &it;
 
-    // Nodes
-    /*
-    for (it = airports.begin(); it != airports.end(); it++) {
-        std::cout << "Name: " << it->second.first.get_name()
-                  << " Abbreviation: " << it->second.first.get_abbreviation()
-                  << " Coords: " << it->second.first.get_coords().first << ", "
-                  << it->second.first.get_coords().second
-                  << " ID = " << it->second.first.get_OpenFlightID() << "\n";
-    }
-    */
+    FlightMap.get_airport_by_ID()
 
-    // Routes
-    int length = routes.size();
-    int i = 0;
-    for (i = 0; i < length; i++) {
-        std::cout << "Source name = " << routes[i].get_source().get_name()
-                  << " Destination = " << routes[i].get_destination().get_name()
-                  << " Code = " << routes[i].get_code()
-                  << " Stops = " << routes[i].get_stops() << "\n";
-    }
-    return 0;
+        // // std::cout << "Left constructor\n";
+        // std::unordered_map<std::string,
+        //                    std::pair<Airport, std::vector<Route>>>::iterator
+        //                    it;
+        // std::cout << "Pointer: " << &it;
+
+        // // Nodes
+        // /*
+        // for (it = airports.begin(); it != airports.end(); it++) {
+        //     std::cout << "Name: " << it->second.first.get_name()
+        //               << " Abbreviation: " <<
+        //               it->second.first.get_abbreviation()
+        //               << " Coords: " << it->second.first.get_coords().first
+        //               << ", "
+        //               << it->second.first.get_coords().second
+        //               << " ID = " << it->second.first.get_OpenFlightID() <<
+        //               "\n";
+        // }
+        // */
+
+        // // Routes
+        // int length = routes.size();
+        // int i = 0;
+        // for (i = 0; i < length; i++) {
+        //     std::cout << "Source name = " <<
+        //     routes[i].get_source().get_name()
+        //               << " Destination = " <<
+        //               routes[i].get_destination().get_name()
+        //               << " Code = " << routes[i].get_code()
+        //               << " Stops = " << routes[i].get_stops() << "\n";
+        // }
+        return 0;
 }
