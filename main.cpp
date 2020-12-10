f6d30c266615c0be02a972bc0370491befda05a6
Ben Civjan <bcivjan2@illinois.edu>
2020-12-10 17:42:46 -0600

    Created main with astar and bfs selection

--- main.cpp
+++ main.cpp
@@ -9,6 +9,11 @@ using std::string;
 int main(int argc, char* argv[]) {
     std::cout << "Program is running\n";
 
+    if (argc <= 1) {
+        cout << "Please run map with one argument" << endl;
+        cout << "Select from: bfs, dijkstra, astar, or all" << endl;
+        return 1;
+    }
     if (argc >= 3) {
         cout << "Please only input one argument" << endl;
         cout << "Select from: bfs, dijkstra, astar, or all" << endl;
@@ -28,35 +33,26 @@ int main(int argc, char* argv[]) {
             cout << "Step: " << i << ", visited: " << output[i].get_name()
                  << endl;
         }
+    } else if (input == "astar") {
+        Astar astar;
+        Graph g("Data/airport_data.csv", "Data/route_data.csv", "3731");
+
+        Airport end = g.get_airport_by_ID("3135");
+
+        vector<Airport> output = astar.astarPath(g, g.start_airport, end);
+
+        cout << "Astar search started" << endl;
+        cout << "Start airport: " << g.start_airport.get_name() << endl;
+        cout << "End airport: " << end.get_name() << endl;
+
+        for (size_t i = 0; i < output.size(); i++) {
+            cout << "Step: " << i << ", visited: " << output[i].get_name()
+                 << endl;
+        }
+        cout << "Reached destination" << endl;
+    } else {
+        cout << "Select from: bfs, dijkstra, astar, or all" << endl;
     }
 
-    // Graph FlightMap("Data/test_airport_data.csv",
-    //                 "Data/test_cycle_route_data.csv");
-    // Graph FlightMap("Data/airport_data.csv", "Data/route_data.csv", "1");
-    // std::unordered_map<std::string, std::pair<Airport, std::vector<Route>>>
-    //     airports = FlightMap.get_airports();
-    // std::vector<Route> routes = FlightMap.get_routes();
-
-    // std::cout << FlightMap.get_adjacent_routes_by_ID("2990").size()
-    //           << std::endl;
-    // std::cout << "Left constructor\n";
-    // std::unordered_map<std::string,
-    //                    std::pair<Airport, std::vector<Route>>>::iterator it;
-    // std::cout << "Pointer: " << &it;
-
-    // std::cout << FlightMap.get_adjacent_routes_by_ID("20").size() <<
-    // std::endl;
-
-    // Routes
-    // int length = routes.size();
-    // int i = 0;
-    // cout << length << endl;
-    // for (i = 0; i < length; i++) {
-    //     std::cout << "Source name = " << routes[i].get_source().get_name()
-    //               << " Destination = " <<
-    //               routes[i].get_destination().get_name()
-    //               << " Code = " << routes[i].get_code()
-    //               << " Stops = " << routes[i].get_stops() << "\n";
-    // }
     return 0;
 }
