6617c094a131991c4ac67ae6ed053b019537178d
Ben Civjan <bcivjan2@illinois.edu>
2020-12-10 17:24:21 -0600

    Working on main

--- main.cpp
+++ main.cpp
@@ -1,17 +1,41 @@
+#include "Astar/astar.h"
+#include "BFS/BFS.h"
 #include "Graph.h"
 
 using std::cout;
 using std::endl;
+using std::string;
 
-int main() {
+int main(int argc, char* argv[]) {
     std::cout << "Program is running\n";
+
+    if (argc >= 3) {
+        cout << "Please only input one argument" << endl;
+        cout << "Select from: bfs, dijkstra, astar, or all" << endl;
+        return 1;
+    }
+
+    string input = string(argv[1]);
+
+    if (input == "bfs") {
+        BFS bfs;
+        Graph g("Data/airport_data.csv", "Data/route_data.csv", "1");
+        vector<Airport> output = bfs.traversal(g, g.start_airport);
+
+        cout << "BFS traversal started" << endl;
+
+        for (size_t i = 0; i < output.size(); i++) {
+            cout << "Step: " << i << ", visited: " << output[i].get_name()
+                 << endl;
+        }
+    }
 
     // Graph FlightMap("Data/test_airport_data.csv",
     //                 "Data/test_cycle_route_data.csv");
-    Graph FlightMap("Data/airport_data.csv", "Data/route_data.csv", "1");
-    std::unordered_map<std::string, std::pair<Airport, std::vector<Route>>>
-        airports = FlightMap.get_airports();
-    std::vector<Route> routes = FlightMap.get_routes();
+    // Graph FlightMap("Data/airport_data.csv", "Data/route_data.csv", "1");
+    // std::unordered_map<std::string, std::pair<Airport, std::vector<Route>>>
+    //     airports = FlightMap.get_airports();
+    // std::vector<Route> routes = FlightMap.get_routes();
 
     // std::cout << FlightMap.get_adjacent_routes_by_ID("2990").size()
     //           << std::endl;
@@ -24,9 +48,9 @@ int main() {
     // std::endl;
 
     // Routes
-    int length = routes.size();
+    // int length = routes.size();
     // int i = 0;
-    cout << length << endl;
+    // cout << length << endl;
     // for (i = 0; i < length; i++) {
     //     std::cout << "Source name = " << routes[i].get_source().get_name()
     //               << " Destination = " <<
