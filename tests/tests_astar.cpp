c10a180e214370a94c9313b9df02d24606afebb2
Ben Civjan <bcivjan2@illinois.edu>
2020-12-10 15:43:42 -0600

    Working on astar tests

--- tests/tests_astar.cpp
+++ tests/tests_astar.cpp
@@ -9,23 +9,25 @@ TEST_CASE("Astar test example", "[part=astar]") {
     Graph g("Data/test_simple_airport_data.csv",
             "Data/test_simple_route_data.csv", "1");
 
-    // astar.astarPath(g, g.start_airport, Airport);
+    Airport end = g.get_airport_by_ID("3");
 
-    // SECTION("Check visited correct number of airports") {
-    //     REQUIRE(bfs.count == 3);
-    // }
+    astar.astarPath(g, g.start_airport, end);
 
-    // SECTION("Check visited correct airport first") {
-    //     REQUIRE(bfs.start_airport.get_OpenFlightID() == "1");
-    // }
+    SECTION("Check visited correct number of airports") {
+        REQUIRE(bfs.count == 3);
+    }
 
-    // SECTION("Check visited correct airport last") {
-    //     REQUIRE(bfs.end_airport.get_OpenFlightID() == "3");
-    // }
+    SECTION("Check visited correct airport first") {
+        REQUIRE(bfs.start_airport.get_OpenFlightID() == "1");
+    }
 
-    // SECTION("Visited airports in correct order") {
-    //     REQUIRE(bfs.visited_airport_ids.at(0) == "1");
-    //     REQUIRE(bfs.visited_airport_ids.at(1) == "2");
-    //     REQUIRE(bfs.visited_airport_ids.at(2) == "3");
-}
+    SECTION("Check visited correct airport last") {
+        REQUIRE(bfs.end_airport.get_OpenFlightID() == "3");
+    }
+
+    SECTION("Visited airports in correct order") {
+        REQUIRE(bfs.visited_airport_ids.at(0) == "1");
+        REQUIRE(bfs.visited_airport_ids.at(1) == "2");
+        REQUIRE(bfs.visited_airport_ids.at(2) == "3");
+    }
 }
